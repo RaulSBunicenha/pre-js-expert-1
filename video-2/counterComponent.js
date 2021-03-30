83f784539e742ba4bf6dd09b4e815bd1183a151b
Raul Bunicenha <raulbunicenha@gmail.com>
2021-03-30 14:16:20 -0300

    Clousure All Counter Component

--- video-2/counterComponent.js
+++ video-2/counterComponent.js
@@ -1,74 +1,80 @@
-const RESTART_BTN = 'btnRestart'
-const COUNTER = 'counter'
-const COUNTER_VALUE = 100
-const INTERVAL_PERIOD = 100
+// IIFE - Immediately Invoked Function Expression
+(() => {
 
-class CounterComponent {
-    constructor () {
-        this.init()   
-    }
+    const RESTART_BTN = 'btnRestart'
+    const COUNTER = 'counter'
+    const COUNTER_VALUE = 100
+    const INTERVAL_PERIOD = 100
 
-    getCounterProxy () {
-        const handler = {
-            set: (currentContext, propertyKey, newValue) => {
-                if (!currentContext.value) currentContext.stopCounter()
-                else currentContext[propertyKey] = newValue
-                
-                return true
+    class CounterComponent {
+        constructor () {
+            this.init()   
+        }
+
+        getCounterProxy () {
+            const handler = {
+                set: (currentContext, propertyKey, newValue) => {
+                    if (!currentContext.value) currentContext.stopCounter()
+                    else currentContext[propertyKey] = newValue
+                    
+                    return true
+                }
+            }
+
+            return new Proxy({
+                value: COUNTER_VALUE,
+                stopCounter: () => {}
+            }, handler)
+        }
+
+        updateText ({ counterElement, counter }) {
+            const identify = '$$counter'
+            const standardText = `Starting in <strong>${identify}</strong>`
+            
+            return () => counterElement.innerHTML = standardText.replace(identify, counter.value--)
+        }
+
+        getCounterStop ({ counterElement, intervalId }) {
+            return () => {
+                clearInterval(intervalId)
+
+                counterElement.innerHTML = ''
+                this.disableBtn(false)
             }
         }
 
-        return new Proxy({
-            value: COUNTER_VALUE,
-            stopCounter: () => {}
-        }, handler)
-    }
+        setButton (btnElement, startFunction) {
+            btnElement.addEventListener('click', startFunction)
 
-    updateText ({ counterElement, counter }) {
-        const identify = '$$counter'
-        const standardText = `Starting in <strong>${identify}</strong>`
-        
-        return () => counterElement.innerHTML = standardText.replace(identify, counter.value--)
-    }
-
-    getCounterStop ({ counterElement, intervalId }) {
-        return () => {
-            clearInterval(intervalId)
-
-            counterElement.innerHTML = ''
-            this.disableBtn(false)
-        }
-    }
-
-    setButton (btnElement, startFunction) {
-        btnElement.addEventListener('click', startFunction)
-
-        return (value = true) => {
-            const attr = 'disabled'
-            if (value) {
-                btnElement.setAttribute(attr, value)
-                return;
+            return (value = true) => {
+                const attr = 'disabled'
+                if (value) {
+                    btnElement.setAttribute(attr, value)
+                    return;
+                }
+                btnElement.removeAttribute(attr)
             }
-            btnElement.removeAttribute(attr)
+        }
+
+        init () {
+            const counterElement = document.getElementById(COUNTER)
+            const counter = this.getCounterProxy()
+            
+            const args = { counterElement, counter }
+            const myFunction = this.updateText(args)
+            const intervalId = setInterval(myFunction, INTERVAL_PERIOD)
+
+            {
+                const btnElement = document.getElementById(RESTART_BTN)
+                const disableBtn = this.setButton(btnElement, this.init.bind(this))
+                disableBtn()
+
+                const args = { counterElement, intervalId }
+                const stopCounter = this.getCounterStop.apply({ disableBtn }, [args])
+                counter.stopCounter = stopCounter
+            }
         }
     }
 
-    init () {
-        const counterElement = document.getElementById(COUNTER)
-        const counter = this.getCounterProxy()
-        
-        const args = { counterElement, counter }
-        const myFunction = this.updateText(args)
-        const intervalId = setInterval(myFunction, INTERVAL_PERIOD)
-
-        {
-            const btnElement = document.getElementById(RESTART_BTN)
-            const disableBtn = this.setButton(btnElement, this.init.bind(this))
-            disableBtn()
-
-            const args = { counterElement, intervalId }
-            const stopCounter = this.getCounterStop.apply({ disableBtn }, [args])
-            counter.stopCounter = stopCounter
-        }
-    }
-}
+    window.CounterComponent = CounterComponent
+})()
